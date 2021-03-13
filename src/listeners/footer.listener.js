60c6010b5c9f24e06312a04e6e3bc9620140e113
Wilgert Velinga <wilgert@wilgert.nl>
2021-03-13 21:20:52 +0100

    split footer handler in next and previous handlers

--- src/listeners/footer.listener.js
+++ src/listeners/footer.listener.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import { footerHandler } from '../handlers/footer.handler.js';
+import { previousButtonHandler, nextButtonHandler } from '../handlers/footer.handler.js';
 export const addFooterListener = () => {
   const nextButton = document.querySelector('#nextButton');
-  nextButton.addEventListener('click', footerHandler);
+  nextButton.addEventListener('click', nextButtonHandler);
   const prevButton = document.querySelector('#prevButton');
-  prevButton.addEventListener('click', footerHandler);
+  prevButton.addEventListener('click', previousButtonHandler);
 };
